b7dd9258eadc6312e1480229db48e5a4f2572879
miguel.castaneda <miguel.castaneda@parservicios.com>
2017-11-14 12:59:56 -0500

    modificaciones en img icono barra lateral v1.1

--- resources/views/0_includes/menuLateral_Izq_estandar.blade.php
+++ resources/views/0_includes/menuLateral_Izq_estandar.blade.php
@@ -5,7 +5,7 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{asset('img/icono_phoneUp.png')}}" class="img-circle" alt="User">
+                <img src="{{asset('img/icono_phoneUp.png')}}" class="img-responsive" alt="User">
             </div>
             <div class="pull-left info">
                 <p>{{\Auth::user()->nombres}}</p>
