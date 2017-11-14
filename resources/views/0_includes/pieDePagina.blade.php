84fcbffd233639d6f312eaa81a4999fc7529fcc4
miguel.castaneda <miguel.castaneda@parservicios.com>
2017-11-14 10:31:30 -0500

    modificaciones en footer v1.0

--- resources/views/0_includes/pieDePagina.blade.php
+++ resources/views/0_includes/pieDePagina.blade.php
@@ -1,7 +1,41 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
+   {{-- <div class="pull-right hidden-xs">
         <b>Version</b> 2.4.0
     </div>
     <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-    reserved.
+    reserved.--}}
+    <div class="row">
+        <div class="col-sm-12 col-lg-8">
+            <h4><strong>Contactenos</strong></h4>
+            <div class="row">
+                <div class="col-lg-4">
+                    Telefonos: <span class="text-muted">300 5755967 - (+57) 7401349</span><br>
+                    Correo: <span class="text-muted">abernalg@phoneup.com</span>
+                </div>
+                <div class="col-lg-4" style="border-left: solid 1px">
+                    <div class="hidden-lg"><br></div>
+                    Telefonos: <span class="text-muted">319 5505453 - (+57) 7401349</span><br>
+                    Correo: <span class="text-muted">mcastanedava@phoneup.com</span>
+                </div>
+                <div class="col-lg-4" style="border-left: solid 1px">
+                    <div class="hidden-lg"><br></div>
+                    Telefonos: <span class="text-muted">305 7135701 - (+57) 7401349</span><br>
+                    Correo: <span class="text-muted">obellog@phoneup.com</span>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-sm-12 col-lg-2">
+            <h4><strong>Siganos en:</strong></h4>
+            <a href="#"><img src="{{asset('img/icono_facebook.png')}}" alt="facebook" width="35px"></a>
+            <a href="#"><img src="{{asset('img/icono_linkedin.png')}}" alt="linkedin" width="35px"></a>
+            <a href="#"><img src="{{asset('img/icono_youtube.png')}}" alt="youTube" width="35px"></a>
+        </div>
+
+        <div class="col-sm-12 col-lg-2">
+            <div class="pull-right full-opacity-hover">
+                <img src="{{asset('img/logo_phoneUp_contraste.png')}}" alt="PhoneUp" width="150px" style="margin-top: 50px">
+            </div>
+        </div>
+    </div>
 </footer>
